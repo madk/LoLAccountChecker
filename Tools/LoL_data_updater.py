1cbeae5e40b31c54e1f32e7777d11b86663c579d
yokrysty <yokrysty@users.noreply.github.com>
2016-06-03 15:29:53 +0300

    - update for patch 6.11 - update python script for scraping LoL data

--- Tools/LoL_data_updater.py
+++ Tools/LoL_data_updater.py
@@ -1,18 +1,32 @@
-import os, urllib, urllib2, json
+import os, sys, urllib, urllib2, json
 
-champs_vers = '6.6.1'
-images_vers = '6.6.1'
-runes_vers = '6.6.1'
-
-base_url = 'http://ddragon.leagueoflegends.com/cdn/'
-champs_url = '%s%s/data/en_US/championFull.json' % (base_url, champs_vers)
-img_url = '%s%s/img/champion/' % (base_url, images_vers)
-runes_url = '%s%s/data/en_US/rune.json' %(base_url, runes_vers)
+api_version_url = 'https://ddragon.leagueoflegends.com/api/versions.json'
+realm_version_url = 'https://ddragon.leagueoflegends.com/realms/na.json'
 
 def loadJsonFromUrl(url):
 	r = urllib2.urlopen(url)
 	str_r = r.read().decode('utf-8')
 	return json.loads(str_r)
+
+print 'Latest API Version: ' + loadJsonFromUrl(api_version_url)[0]
+
+obj = loadJsonFromUrl(realm_version_url)
+
+champs_vers = obj['n']['champion']
+runes_vers = obj['n']['rune']
+
+base_url = 'http://ddragon.leagueoflegends.com/cdn/'
+champs_url = '%s%s/data/en_US/championFull.json' % (base_url, champs_vers)
+runes_url = '%s%s/data/en_US/rune.json' %(base_url, runes_vers)
+
+print '\nRealm: NA'
+
+print 'Champions: ' + champs_vers
+print 'Runes: ' + runes_vers
+
+print '\nDownload? y/n'
+if raw_input() != 'y':
+    sys.exit()
 
 obj = loadJsonFromUrl(champs_url)
 
@@ -22,9 +36,9 @@ for ck, champion in sorted(obj['data'].items()):
     skins = []
 
     for skin in champion['skins']:
-        skins.append({'Id': int(skin['id']), 'Name': skin['name'], 'Num': skin['num']})
+        skins.append({'Id': int(skin['id']), 'Name': skin['name'], 'Num': skin['num'], 'ChampionId': int(champion['key'])})
 
-    output.append({'Id': int(champion['key']), 'StrId': champion['id'], 'Name': champion['name'], 'Skins': skins})
+    output.append({'Id': int(champion['key']), 'Name': champion['id'], 'DisplayName': champion['name'], 'Skins': skins})
 
     print(champion['name'])
 
@@ -34,23 +48,6 @@ with open('Champions.json', 'w') as f:
 
 with open('Champions.Version', 'w') as f:
 	f.write(champs_vers)
-
-#if not os.path.exists('images'):
-#	os.makedirs('images')
-
-'''for champion in output:
-	filename = champion['StrId'] + '.png'
-	
-	url = img_url + filename
-	
-	filepath = os.path.join('images', filename)
-	
-	urllib.urlretrieve(url, filepath)
-	
-	print 'Downloading image for champion ' + champion['Name']'''
-
-#with open('Images.Version', 'w') as f:
-#	f.write(images_vers)
 
 def getRuneType(str_type):
 	type = {
@@ -84,5 +81,5 @@ with open('Runes.json', 'w') as f:
 with open('Runes.Version', 'w') as f:
 	f.write(runes_vers)
 
-print "Press Enter to continue..." 
+print 'Press Enter to continue...' 
 raw_input()
